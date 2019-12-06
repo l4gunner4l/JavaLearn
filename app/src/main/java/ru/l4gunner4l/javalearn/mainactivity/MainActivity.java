1feb9c64a10febaf25367977cc955bc56ddc2fd9
devBeginer <sFr0@ya.ru>
2019-12-06 16:19:54 +0300

    #12 Toolbar layout fixed

--- app/src/main/java/ru/l4gunner4l/javalearn/mainactivity/MainActivity.java
+++ app/src/main/java/ru/l4gunner4l/javalearn/mainactivity/MainActivity.java
@@ -2,7 +2,11 @@ package ru.l4gunner4l.javalearn.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//import android.support.v7.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import ru.l4gunner4l.javalearn.R;
 
@@ -16,9 +20,25 @@ import ru.l4gunner4l.javalearn.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
     }
 }
